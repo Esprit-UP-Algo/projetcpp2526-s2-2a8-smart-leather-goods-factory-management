3cb7b0e32e1dc574268b78234808e275d9fccffd
LYNA0509 <lynamabrouk0905@gmail.com>
2026-03-26 16:37:14 +0100

    chore: supprimer dossier crud client

--- crud client/matiere.cpp
+++
@@ -1,8 +0,0 @@
-#include "matiere.h"
-
-Matiere::Matiere() {}
-
-Matiere::Matiere(const QString &module, const QString &reference, const QString &type,
-                 const QString &quantite, const QString &seuil, const QDate &dateExp)
-    : m_module(module), m_reference(reference), m_type(type),
-      m_quantite(quantite), m_seuil(seuil), m_dateExpiration(dateExp) {}
